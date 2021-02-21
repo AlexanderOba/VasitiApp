7430e21048ebb74c7ca3d42328660e2a2cb4c099
AlexanderOba <alexzy4ak@gmail.com>
2021-02-21 08:54:13 -0800

    ran build

---
+++ build/precache-manifest.d9d74c9b49634d022020b3bb0e8188d1.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c2cecd8e509e74682344f81500290f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b273353b7a795a085637",
+    "url": "/static/css/main.644d15d1.chunk.css"
+  },
+  {
+    "revision": "1a8a1d52ea75f1333ebe",
+    "url": "/static/js/2.6e2d97bb.chunk.js"
+  },
+  {
+    "revision": "47f0823269ddc1f531d85f48617139aa",
+    "url": "/static/js/2.6e2d97bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b273353b7a795a085637",
+    "url": "/static/js/main.15ebbdeb.chunk.js"
+  },
+  {
+    "revision": "e731de3a879614fc0df9",
+    "url": "/static/js/runtime-main.066e2905.js"
+  },
+  {
+    "revision": "d0215b9a7002845aecbfb56e60456b38",
+    "url": "/static/media/adetola.d0215b9a.png"
+  },
+  {
+    "revision": "224a1987dd35ad198cc39b37879c92b0",
+    "url": "/static/media/adeyemi.224a1987.png"
+  },
+  {
+    "revision": "1224ea69629d03182ed93f7b7e2d4dad",
+    "url": "/static/media/amos.1224ea69.png"
+  },
+  {
+    "revision": "5af83dfab3be2a02f239fb6db0a49ccc",
+    "url": "/static/media/atm-lady.5af83dfa.png"
+  },
+  {
+    "revision": "344300657a1162d1609bd299936775d7",
+    "url": "/static/media/chidi.34430065.png"
+  },
+  {
+    "revision": "0b33ad4d9e9de4a693dd81e190814c38",
+    "url": "/static/media/chisom.0b33ad4d.png"
+  },
+  {
+    "revision": "6cd226cb2bca2c346f8a8d34b9188005",
+    "url": "/static/media/cone.6cd226cb.png"
+  },
+  {
+    "revision": "e8ecda86b8e0b9526de6313590d8ac00",
+    "url": "/static/media/emmanuel.e8ecda86.png"
+  },
+  {
+    "revision": "c00eeb7a891d17e7dbb09bfd3a02912c",
+    "url": "/static/media/fairguy.c00eeb7a.png"
+  },
+  {
+    "revision": "5f879ed7e15f1765b751e3fe7ee8edc5",
+    "url": "/static/media/feyi.5f879ed7.png"
+  },
+  {
+    "revision": "53bb766018b083c925ba2cddfe6dd543",
+    "url": "/static/media/joseph.53bb7660.png"
+  },
+  {
+    "revision": "fd0897f09daf9a3d1d7f633f6e7875cc",
+    "url": "/static/media/karen.fd0897f0.png"
+  },
+  {
+    "revision": "9460893e4a4572d05c9032456e64a309",
+    "url": "/static/media/messagecone.9460893e.png"
+  },
+  {
+    "revision": "9abadee07fe47651ea7b42089ae3ef62",
+    "url": "/static/media/mobile.9abadee0.png"
+  },
+  {
+    "revision": "e08a88d83af1ecfdc2e7865c57c00404",
+    "url": "/static/media/models.e08a88d8.png"
+  },
+  {
+    "revision": "91d5021b513ffac99c328ed5dd588b87",
+    "url": "/static/media/oluchi.91d5021b.png"
+  },
+  {
+    "revision": "caa92dad598bda4272357d18c4cec0e8",
+    "url": "/static/media/promise.caa92dad.png"
+  },
+  {
+    "revision": "a0c48df389cb9d08e227f1aa82cf4b93",
+    "url": "/static/media/temi.a0c48df3.png"
+  }
+]);
